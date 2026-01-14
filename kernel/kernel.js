2ddf352dd24dc8885d1a1eafd762924c26e34984
Purplegon2 <dnljdavid2012@gmail.com>
2026-01-14 11:53:21 -0600

    Update kernel.js

--- kernel/kernel.js
+++ kernel/kernel.js
@@ -1,2 +1,6 @@
-export function parse() {
+export function parse(command) {
+  var cmd_type = command.split(".")[0]
+  var cmd = command.split(".")[1].split("(")[0]
+  var command = command.split(".")[1].split("(")[1].replace("'", "")
+  
 }
